4a56a6c91e162d5f93366fa2a24f9cec85b8cf6d
Raykrad <109039576+Raykrad@users.noreply.github.com>
2024-05-20 09:41:23 +0300

    Create includes.cpp

---
+++ includes/includes.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <stdexcept>
+#include <iomanip>
+#include <sstream>
+#include <ctime>
